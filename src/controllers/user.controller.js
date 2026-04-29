29dc3bfc237bdb3cd37cf3d5e7252ae269fc8eb5
Fossele <fosselewilfried@gmail.com>
2026-04-30 00:23:13 +0100

    added loggedOut feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,52 +1,66 @@
 import { User } from "../model/user.model.js";
 
-const registerUser = async(req, res) =>{
+const registerUser = async (req, res) => {
     try {
-      const {username , email, password} = req.body;
+        const { username, email, password } = req.body;
 
-      //basic validation
+        //basic validation
 
-      if(!username || !password || !email){
-       // console.log("username, password and email are all required.");
-        return res.status(400).json({message :"username, password and email are all required." });
-      }
+        if (!username || !password || !email) {
+            // console.log("username, password and email are all required.");
+            return res.status(400).json({ message: "username, password and email are all required." });
+        }
 
-      // check if user exist
+        // check if user exist
 
-      const existing = await User.findOne({email: email.toLowerCase()});
+        const existing = await User.findOne({ email: email.toLowerCase() });
 
-    if(existing){
-        return res.status(400).json({message: "User already exists."});
-    }
-       // create user  
-    const user = await User.create({username, email: email.toLowerCase(), password, loggedIn: false,});
-    res.status(201).json({message: "User registered" , user: { id:user._id , email: user.email, username: user.username}});
+        if (existing) {
+            return res.status(400).json({ message: "User already exists." });
+        }
+        // create user  
+        const user = await User.create({ username, email: email.toLowerCase(), password, loggedIn: false, });
+        res.status(201).json({ message: "User registered", user: { id: user._id, email: user.email, username: user.username } });
 
     } catch (error) {
-        return res.status(500).json({message:"internal server failure"});
+        return res.status(500).json({ message: "internal server failure" });
     }
-} 
+}
 
-const loginUser = async(req, res)=>{
+const loginUser = async (req, res) => {
     try {
-    //check if user exist
-    const {email , password} = req.body;
-    const user = await  User.findOne({email: email.toLowerCase()});
-    if(!user){
-        return res.status(400).json({message: 'user not found in our system.'})
-    }
-    const isMatch = await user.comparePassword(password);
-    if(!isMatch) return res.status(400).json({message: 'Invalid password'});
+        //check if user exist
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+            return res.status(400).json({ message: 'user not found in our system.' })
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
 
-    return res.status(200).json({message: "User logged In", user:{id: user._id, email: user.email, username: user.username}});
-     
-    } catch (error){
-        res.status(500).json({message:"Internal server error"});
+        return res.status(200).json({ message: "User logged In", user: { id: user._id, email: user.email, username: user.username } });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
     }
-    
-    
+
+
+}
+
+const logoutUser = async (req, res) => {
+    try {
+
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+            return res.status(400).json({ message: 'user not found in our system.' })
+        }
+        return res.status(200).json({ message: "User logged out"});
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export {
-    registerUser, loginUser
+    registerUser, loginUser, logoutUser
 }
